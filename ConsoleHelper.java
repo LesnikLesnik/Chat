3b4ee646116e8b38c1b94198052234608499f44e
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-08-23 09:37:57 +0300

    Add files via upload

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,7 +1,36 @@
 package com.javarush.task.task30.task3008;
 
-    //вспомогательный класс, для чтения или записи в консоль.
+//вспомогательный класс, для чтения или записи в консоль.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 //вся работа должна проходить через этот класс
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) { //выводим message в консоль
+        System.out.println(message);
+    }
+
+    public static String readString() { //считываем строку с консоли
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            return readString();
+        }
+
+    }
+
+    public static int readInt() { //считываем число из консоли
+        try {
+            return Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            return readInt();
+        }
+    }
+
 }
