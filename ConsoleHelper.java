f2d53be89e571553a2b754440f02cfaaeffcc43d
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-08-23 09:11:06 +0300

    Add files via upload

---
+++ ConsoleHelper.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task30.task3008;
+
+    //вспомогательный класс, для чтения или записи в консоль.
+
+//вся работа должна проходить через этот класс
+public class ConsoleHelper {
+}
